8bc9bee41c96208938a70a83de3e95ab0632e217
Andrew Campbell <acc2023@comcast.net>
2025-03-19 09:51:37 -0500

    minor structural

---
+++ src/backend/SoundPlayer.cpp
@@ -0,0 +1,6 @@
+/*
+ * Author: Andrew Campbell
+ * Date: 03-19-2025
+ */
+
+#include "SoundPlayer.hpp"
---
+++ src/backend/SoundPlayer.hpp
@@ -0,0 +1,14 @@
+/*
+ * Author: Andrew Campbell
+ * Date: 03-19-2025
+ */
+
+#ifndef SOUNDPLAYER_H
+#define SOUNDPLAYER_H
+
+namespace 
+{
+ 
+}
+
+#endif // SOUNDPLAYER_H
